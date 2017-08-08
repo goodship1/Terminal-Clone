54a462d3b81fba6cd53fbc76d220ec260fe9e043
Gavin Goodship <msgoodship1@gmail.com>
2017-08-08 21:04:06 +0100

    Update TestingofShelloperations.py

--- I-LinuxShell/Testing/TestingofShelloperations.py
+++ I-LinuxShell/Testing/TestingofShelloperations.py
@@ -1,61 +1,6 @@
 from shellOperationTesting import ShellOperations
 import os
-class TestForShellOperations(object):
-	def __init__(self):
-		self.shell =  ShellOperations()
-		self.line = ""
-		self.line_test = "abc"
-		self.empty = [""]
-		self.current_format = self.shell.formattingCurrentWorkingDirectory()
-		
-		
-	def do_CurTest(self):
-		assert os.getcwd()==self.shell.do_cur(self.line)
-		return True
-	
-	def testForEmptyLine(self):
-		return self.empty[0] == self.shell.checkiflineisEmpty(self.line)
-	
-	def testForlineNotEmpty(self):
-		assert self.shell.checkiflineisEmpty(self.line)!=self.empty[0]
-		return True
-	
-	def testingCurrentdirectioryfileFormatting(self):
-		Files = os.getcwd()
-		Files = Files.split('/')
-		Files.remove(Files[0])
-		assert Files == self.shell.formattingCurrentWorkingDirectory()
-		return True
-	
-	def testingErrorhandlingofEmptylinedoCdirMethod(self):
-		return self.shell.do_cdir(self.line)== "cannot take a empty line"
-	
-	
-	def testingchangeDirectorydirectory(self):
-		pass
-	
-	def testingdo_bdir(self):
-		back = os.chdir(os.path.pardir)
-		assert back  == self.shell.do_bdir(self.line)
-		return True
-	
-	def testingCheckIfFileisTrue(self):
-		result = os.path.isfile('test.txt')
-		assert result == self.shell.checkIfFile('test.txt')
-		return True
-	
-	def testingCheckIfFilesIsFalse(self,files):
-		result = os.path.isfile(files)
-		assert result == self.shell.checkIfFile(files)
-	
+import unittest
 
-	
-		
-		
-
-	
-		
-
-
-T = TestForShellOperations()
-T.testingCheckIfFilesIsFalse('test.txt')
+class testingForshellFunctions(unit.test.testcase):
+	pass
