f66a04998ae72aeab4415726bb837166737f336d
Gavin Goodship <msgoodship1@gmail.com>
2017-08-09 16:44:34 +0100

    Update TestingofShelloperations.py

--- I-LinuxShell/Testing/TestingofShelloperations.py
+++ I-LinuxShell/Testing/TestingofShelloperations.py
@@ -14,4 +14,15 @@ def testingformatCurrentWorkingDirectory():
 	Files = Files.split('/')
 	Files.remove(Files[0])
 	assert Files ==  shell.formattingCurrentWorkingDirectory()
-	
+
+def testingcheckifFile():
+	assert shell.checkIfFile('test.txt')==True
+
+def testingreturnofdo_cw():
+	assert shell.do_cw('test.txt') == 3
+
+def testingErrorhandlingofdo_cw():
+	assert shell.do_cw('wgwgfwgg')=="not a file"
+
+def testingdo_show():
+	assert shell.do_show('test.txt') == True
