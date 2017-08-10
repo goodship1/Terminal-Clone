caa90c3bc26218ee49b19ce206d2d85c4e5553c0
Gavin Goodship <msgoodship1@gmail.com>
2017-08-10 15:50:52 +0100

    Update TestingofShelloperations.py

--- I-LinuxShell/Testing/TestingofShelloperations.py
+++ I-LinuxShell/Testing/TestingofShelloperations.py
@@ -26,3 +26,36 @@ def testingErrorhandlingofdo_cw():
 
 def testingdo_show():
 	assert shell.do_show('test.txt') == True
+
+def testingdo_showErrorHandling():
+	assert shell.do_show('adadada') == "not a file"
+
+def testingofdo_cur():
+	current = os.getcwd()
+	assert current ==  shell.do_cur(line='')
+
+def testingDo_h():
+	assert shell.do_h(line="")==True
+
+def testingdo_geterrorHandling(line = ""):
+	#testing when line is empty
+	assert shell.do_get(line) ==  "cannot take a empty line"
+
+def testingdo_get(line = 'update'):
+	#testing do_ get update
+	assert shell.do_get(line)==True
+
+def testingdo_get(line = "python"):
+	#testing if we can find another package 
+	assert shell.do_get(line)==True
+
+def testingdo_bl():
+	files = os.listdir(os.getcwd())
+	assert files == shell.do_bl(line ="")
+
+def testingdo_cdir(line = ""):
+	#testing do_cdir with empty line 
+	#should return error handling
+	assert shell.do_cdir(line) ==  "cannot take a empty line"
+
+
