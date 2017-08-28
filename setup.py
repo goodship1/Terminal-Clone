229c93184314a26a54ab0e0f166960e89b168fcd
Gavin Goodship <msgoodship1@gmail.com>
2017-08-28 19:29:47 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,17 @@
-import os
+import setuptools
 
-version = 1.0
+Version = 1.0
+
+if __name__ == 'main':
+  setuptools.setup(
+    name = i-linux
+    version = Version
+    
+    
+    
+    
+    
+    
+    
+    
+    
