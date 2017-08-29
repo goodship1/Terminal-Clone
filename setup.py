2d6a93d97ec454d292d45b5616c9ff468268f580
Gavin Goodship <msgoodship1@gmail.com>
2017-08-29 15:01:16 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ if __name__ == 'main':
   setuptools.setup(
     name = i-linux
     version = Version
+    description = "basic interactive linux shell using prompt-toolkit
+    author = "gavin goodship"
+  )
     
     
     
