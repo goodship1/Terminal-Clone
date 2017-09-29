32c8fa4c3008d9d025738150315c7dcb484a61b2
Gavin Goodship <msgoodship1@gmail.com>
2017-09-29 15:11:33 +0100

    Update keys.py

--- I-LinuxShell/keys.py
+++ I-LinuxShell/keys.py
@@ -1,5 +1,7 @@
 from prompt_toolkit.key_binding.defaults import load_key_bindings_for_prompt
 from prompt_toolkit.keys import Keys
 
-class  Keybindings(object):
-  pass
+class  _Keybindings(object):
+    register = load_key_bindings_for_prompt()
+    
+  
