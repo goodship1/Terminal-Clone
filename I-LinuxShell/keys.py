1ae8cde20a6c267ebbf5b201bef0d711b70e64ae
Gavin Goodship <msgoodship1@gmail.com>
2017-10-05 18:34:49 +0100

    adding keybindings to shell

--- I-LinuxShell/keys.py
+++ I-LinuxShell/keys.py
@@ -1,9 +1,13 @@
+import os
 from prompt_toolkit.key_binding.defaults import load_key_bindings_for_prompt
 from prompt_toolkit.keys import Keys
 
-class  Keybindings(object):
-    register = load_key_bindings_for_prompt()
-    
-  @register.add_bindings(Keys.H)
-  def _(event):
-        pass
+class KeyBindings(object):
+	
+	register = load_key_bindings_for_prompt()
+	
+	@register.add_binding(Keys.ControlE)
+	def _(event):
+        """control E exits the shell"""
+		event.cli.run_in_terminal(os._exit(0))
+	
