69b818194b8d5953095f6cf7ab93fa17908330ad
Gavin Goodship <msgoodship1@gmail.com>
2017-10-30 12:38:44 +0000

    updating toolbar

--- I-LinuxShell/toolbar.py
+++ I-LinuxShell/toolbar.py
@@ -9,4 +9,4 @@ class ToolBar(object):
 		})
     
     def get_bottom_toolbar_tokens(self,cli):
-      return [(Token.Toolbar,"Control-S [SH]")]
+      return [(Token.Toolbar,"Control-E [Exit] Control-S [SH] Control-T [tod] Control-A [shows all commands]")]
